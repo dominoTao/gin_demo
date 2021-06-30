69c00055d958a71e18de6ba93918b4eb0041f16f
javataotao <javataotao@gmail.com>
2021-06-30 20:33:56 +0800

    route group

--- lesson17/main.go
+++ lesson17/main.go
@@ -6,6 +6,21 @@ import (
 )
 func main() {
 	r := gin.Default()
+	// 路由组
+	// 把公用的前缀提取出来 创建一个路由组
+	group := r.Group("/view")
+	{
+		group.GET("/a", func(c *gin.Context) {
+
+		})
+		group.GET("/b", func(c *gin.Context) {
+
+		})
+		group.GET("/c", func(c *gin.Context) {
+
+		})
+	}
+
 	r.GET("/index", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"method":"GET",
@@ -26,5 +41,28 @@ func main() {
 			"method":"DELETE",
 		})
 	})
-	r.Run(":80")
+	// any 请求方法的大集合
+	r.Any("/ii", func(c *gin.Context) {
+		switch c.Request.Method {
+		case "GET":
+			c.JSON(http.StatusOK, gin.H{
+				"method":"GET",
+			})
+		case http.MethodPost:
+			c.JSON(http.StatusOK, gin.H{
+				"method":"POST",
+			})
+			// ...
+		}
+	})
+	r.HEAD("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method":"HEAD",
+		})
+	})
+	r.LoadHTMLFiles("./views/404.html")
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "./views/404.html", nil)
+	})
+	r.Run(":8080")
 }
