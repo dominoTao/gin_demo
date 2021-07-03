d30f12b5e710d7fc9efe98988402f90ac3f80ab4
javataotao <javataotao@gmail.com>
2021-07-03 16:32:46 +0800

    create column

--- lesson19/main.go
+++ lesson19/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// 连接数据库
 	//想要正确的处理 time.Time ，您需要带上 parseTime 参数， (更多参数) 要支持完整的 UTF-8 编码，您需要将 charset=utf8 更改为 charset=utf8mb4
-	dsn := "root:root@tcp(127.0.0.1:3305)/db1?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:xxx@tcp(127.0.0.1:3305)/db1?charset=utf8mb4&parseTime=True&loc=Local"
 	// 简单连接
 	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	//MySQl 驱动程序提供了 一些高级配置 可以在初始化过程中使用，例如：
@@ -36,9 +36,13 @@ func main() {
 // 创建表 自动迁移 把结构体和表进行对应
 	db.AutoMigrate(&UserInfo{})
 	// 创建数据对象
-	info := UserInfo{1, "小黄", "男", "蛙泳"}
-	db.Create(&info)
+	//info := UserInfo{1, "小黄", "男", "蛙泳"}
+	//db.Create(&info)
 	//defer
+	var users = []UserInfo{{Name: "jinzhu1", Gender: "男", Hobby: ""},
+		{Name: "jinzhu2", Gender: "男", Hobby: ""},
+		{Name: "jinzhu3", Gender: "女", Hobby: ""}}
+	db.Create(&users)
 }
 
 // 对应数据表
